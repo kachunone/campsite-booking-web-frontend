119583b500f5fd287acc71798dab6255b8dd1774
morris <kajimone@gmail.com>
2023-04-16 02:40:33 -0400

    fixed minor bugs of navbar and navLink

--- src/shared/components/NavLinks.tsx
+++ src/shared/components/NavLinks.tsx
@@ -3,17 +3,36 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "./NavLinks.css";
+import { Link } from "react-router-dom";
 
 const NavLinks: React.FC = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" variant="dark" className="navbar">
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      variant="dark"
+      className="navbar"
+      fixed="top"
+    >
       <Container>
         <Navbar.Brand href="#home">Campscape</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="#login">Login</Nav.Link>
-            <Nav.Link href="#signup">Sign Up</Nav.Link>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to={"/landing"}>
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to={"/choosing"}>
+              Campgrounds
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link as={Link} to={"/login"}>
+              Login
+            </Nav.Link>
+            <Nav.Link as={Link} to={"/signup"}>
+              Sign Up
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
