8808b1c9dd908adc60fe4762ff9cfae68711e5c9
Morris Wan <102487449+kachunone@users.noreply.github.com>
2023-05-24 16:49:14 -0400

    modified bookingPage's layout

--- src/pages/Booking/booking.tsx
+++ src/pages/Booking/booking.tsx
@@ -74,7 +74,7 @@ const Booking: React.FC = () => {
   return (
     <Container fluid className="booking">
       <Row className="detail-card-container" style={{ marginTop: "5rem" }}>
-        <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12}>
+        <Col xs={12} sm={12} md={12} lg={12} xl={5} xxl={5}>
           <Card.Img
             style={{
               borderRadius: "5px",
