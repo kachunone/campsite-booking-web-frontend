f4c4d53afcffddf0d5bc23a21c93f0c2ce6f719a
Morris Wan <102487449+kachunone@users.noreply.github.com>
2023-05-25 18:07:21 -0400

    fixed auth route bug

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -7,17 +7,22 @@ import { useNavigate } from "react-router-dom";
 import { Modal } from "react-bootstrap";
 
 const Auth: React.FC = () => {
-  const auth = useContext(AuthContext);
-  const { state } = useLocation();
-  const navigate = useNavigate();
-  const isLoginMode = state.isLoginMode;
-
   //Modal setup
   const [show, setShow] = useState(false);
   const [header, setHeader] = useState("");
   const [prompt, setPrompt] = useState("");
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+
+  const auth = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const { state } = useLocation();
+  const isNavigated: boolean = state && state.isLoginMode;
+  if (!isNavigated) {
+    return <Navigate to="/landing" replace />;
+  }
+
+  const isLoginMode = state.isLoginMode;
 
   const handleSubmit = async (formData: any, isLoginMode: boolean) => {
     if (!formData.email || !formData.password) {
@@ -36,37 +41,39 @@ const Auth: React.FC = () => {
 
     const action = isLoginMode ? "login" : "signup";
 
-    const response = await fetch(
-      `${process.env.REACT_APP_BASE_URL}/api/auth/${action}`,
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
-      }
-    );
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_BASE_URL}/api/auth/${action}`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formData),
+        }
+      );
 
-    if (response.status === 200) {
-      const { token, username } = await response.json();
-      sessionStorage.setItem("token", token);
-      auth.login();
-      const formattedUsername =
-        username.charAt(0).toUpperCase() + username.slice(1).toLowerCase();
-      auth.setLoggedInUser(formattedUsername);
-      navigate("/choosing", { replace: true });
-    } else if (response.status === 201) {
-      navigate("/auth", {
-        state: { isLoginMode: true },
-        replace: true,
-      });
-      setHeader("Account created");
-      setPrompt("You can log in with your account now!");
-      setShow(true);
-    } else {
-      const { message } = await response.json();
-      setHeader("Error");
-      setPrompt(message);
-      setShow(true);
-    }
+      if (response.status === 200) {
+        const { token, username } = await response.json();
+        sessionStorage.setItem("token", token);
+        auth.login();
+        const formattedUsername =
+          username.charAt(0).toUpperCase() + username.slice(1).toLowerCase();
+        auth.setLoggedInUser(formattedUsername);
+        navigate("/choosing", { replace: true });
+      } else if (response.status === 201) {
+        navigate("/auth", {
+          state: { isLoginMode: true },
+          replace: true,
+        });
+        setHeader("Account created");
+        setPrompt("You can log in with your account now!");
+        setShow(true);
+      } else {
+        const { message } = await response.json();
+        setHeader("Error");
+        setPrompt(message);
+        setShow(true);
+      }
+    } catch (error) {}
   };
 
   return (
